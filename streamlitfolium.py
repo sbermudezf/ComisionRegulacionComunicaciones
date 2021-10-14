d369bb56e24f2ea395d3f0d8fc0bbc5c89890e98
Santiago Bermúdez Feijóo <59633494+sbermudezf@users.noreply.github.com>
2021-10-14 10:47:35 -0500

    Update streamlitfolium.py

--- streamlitfolium.py
+++ streamlitfolium.py
@@ -1,4 +1,16 @@
-pip install streamlit-folium
+import subprocess
+import sys
+import pip
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+# Example
+if __name__ == '__main__':
+    install('streamlit')
 import streamlit as st
 from streamlit_folium import folium_static
 import folium
