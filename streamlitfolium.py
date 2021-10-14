2bea57d34986aa1637ac40774fac982d69205602
Santiago Bermúdez Feijóo <59633494+sbermudezf@users.noreply.github.com>
2021-10-14 14:27:03 -0500

    Update streamlitfolium.py

--- streamlitfolium.py
+++ streamlitfolium.py
@@ -42,5 +42,22 @@ source_code = HtmlFile.read()
 print(source_code)
 components.html(source_code,height=600,width=1000)
 
-df2=pd.read_csv("INTERNET_MOVIL_DEMANDA_ABONADOS_7.csv",delimiter=';')
-st.write(df2)
+from urllib.request import urlopen
+import json
+
+resource_idvozmovilAB = '3a9c0304-3795-4c55-a78e-079362373b4d'
+
+url_envios_vozmovilAB = 'https://www.postdata.gov.co/api/action/datastore/search.json?resource_id=' + resource_idvozmovilAB + '' \
+                       '&fields[]=anno&fields[]=trimestre&fields[]=modalidad'\
+                       '&group_by=anno,trimestre,modalidad'\
+                       '&sum=abonados' 
+
+responsevozmovilAB = urlopen(url_envios_vozmovilAB + '&limit=10000') # Se obtiene solo un registro para obtener el total de registros en la respuesta
+json_envios_vozmovilAB = json.loads(responsevozmovilAB.read())
+Total_registrosvozmovilAB = json_envios_vozmovilAB['result']['total']
+ResponsevozmovilAB = urlopen(url_envios_vozmovilAB + '&limit=' + str(Total_registrosvozmovilAB))
+Abonados_vozmovilAB = json.loads(ResponsevozmovilAB.read())
+# Extracción de registros del json
+vozmovilAB = pd.DataFrame(Abonados_vozmovilAB['result']['records'])
+
+st.write(vozmovilAB)
